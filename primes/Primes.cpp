2698cdb590b87de7ce34a27064d9e22b666aec61
Ajay MT <ajay.tatachar@gmail.com>
2020-05-27 11:34:19 -0500

    generate 64-bit numbers, test multiple numbers in each chare

--- primes/Primes.cpp
+++ primes/Primes.cpp
@@ -2,48 +2,55 @@
 #include <cmath>
 #include <utility>
 #include <cstdlib>
+#include <cstdint>
 #include "Primes.decl.h"
 
 class Main : public CBase_Main
 {
 private:
-  std::pair<long, bool> *primes;
+  std::pair<uint64_t, bool> *primes;
   int count;
   int completed = 0;
   double start_time, end_time;
 public:
   Main(CkArgMsg *m);
-  void result(int index, bool isPrime);
+  void result(int index, int m, bool *arePrime);
 };
 
-Main::Main(CkArgMsg *m)
+Main::Main(CkArgMsg *msg)
 {
-  if (m->argc < 2) {
-    ckout << "argument required" << endl;
+  if (msg->argc < 3) {
+    ckout << "2 arguments required" << endl;
     CkExit();
   }
 
+  count = std::atoi(msg->argv[1]);
+  int m = std::atoi(msg->argv[2]);
+  primes = new std::pair<uint64_t, bool>[count];
   start_time = CkTimer();
-  count = std::atoi(m->argv[1]);
-  primes = new std::pair<long, bool>[count];
-  for (int i = 0; i < count; ++i) {
-    long n = rand();
-    primes[i] = std::make_pair(n, false);
-    CProxy_PrimeChecker::ckNew(i, n, thisProxy);
+
+  for (int i = 0; i < count; i += m) {
+    uint64_t *nums = new uint64_t[m];
+    int j = 0;
+    for (; j < m && j + i < count; ++j) {
+      uint64_t n = ((uint64_t)rand() << 32) | rand();
+      primes[i + j] = std::make_pair(n, false);
+      nums[j] = n;
+    }
+    CProxy_PrimeChecker::ckNew(i, j, nums, thisProxy);
+    delete[] nums;
   }
 }
 
-void Main::result(int index, bool isPrime)
+void Main::result(int index, int m, bool *results)
 {
-  ++completed;
-  primes[index].second = isPrime;
+  completed += m;
+  for (int i = 0; i < m; ++i)
+    primes[index + i].second = results[i];
+
   if (completed == count) {
     end_time = CkTimer();
-    ckout
-      << "start: " << start_time
-      << " end: " << end_time
-      << " diff: " << (end_time - start_time)
-      << endl;
+    ckout << (end_time - start_time) << endl;
     delete[] primes;
     CkExit();
   }
@@ -52,18 +59,18 @@ void Main::result(int index, bool isPrime)
 class PrimeChecker : public CBase_PrimeChecker
 {
 private:
-  int isPrime(const long number);
+  int isPrime(const uint64_t number);
 public:
-  PrimeChecker(int index, long n, CProxy_Main main);
+  PrimeChecker(int index, int m, uint64_t *nums, CProxy_Main main);
 };
 
-int PrimeChecker::isPrime(const long number)
+int PrimeChecker::isPrime(const uint64_t number)
 {
   if (number <= 1) return 0;
   if (number == 2) return 1;
   if (number % 2 == 0) return 0;
 
-  for (int i = 3; i < number / 2; i += 2) {
+  for (int i = 3; i <= number / i; i += 2) {
     if (0 == number % i)
       return 0;
   }
@@ -71,9 +78,13 @@ int PrimeChecker::isPrime(const long number)
   return 1;
 }
 
-PrimeChecker::PrimeChecker(int index, long n, CProxy_Main main)
+PrimeChecker::PrimeChecker(int index, int m, uint64_t *nums, CProxy_Main main)
 {
-  main.result(index, isPrime(n));
+  bool *results = new bool[m];
+  for (int i = 0; i < m; ++i)
+    results[i] = isPrime(nums[i]);
+  main.result(index, m, results);
+  delete[] results;
 }
 
 #include "Primes.def.h"
