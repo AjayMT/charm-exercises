834553547603a0361be5f578f4d24b80e80ae290
Ajay MT <ajay.tatachar@gmail.com>
2020-06-03 01:14:11 -0500

    [wip] odd-even sort sdag

---
+++ odd-even-sdag/odd_even.cpp
@@ -0,0 +1,111 @@
+
+#include "odd_even.decl.h"
+#include <cstdlib>
+#include <vector>
+
+
+class Main : public CBase_Main
+{
+private:
+  int n;
+  CProxy_Elem elems;
+
+public:
+  Main(CkArgMsg *m);
+  void done()
+    { elems.verify(0, true); }
+  void verified()
+    { CkExit(); }
+};
+
+Main::Main(CkArgMsg *m)
+{
+  if (m->argc < 2) {
+    ckout << "1 argument required" << endl;
+    CkExit();
+  }
+  n = std::atoi(m->argv[1]);
+  elems = CProxy_Elem::ckNew(thisProxy, n, n);
+}
+
+
+class Elem : public CBase_Elem
+{
+  Elem_SDAG_CODE;
+private:
+  CProxy_Main main;
+  CProxyElement_Elem self;
+  int n;
+  int num;
+  int stage = 0;
+
+  // this chare's value should be greater than its partner's when:
+  // - its index is even and it is on an odd stage
+  //   (i.e its partner is the odd chare to its left)
+  // - its index is odd and it is on an even stage
+  //   (its partner is the even number to its left)
+  int should_be_greater()
+    { return (stage + thisIndex) % 2 != 0; }
+
+  int partner(int s)
+    {
+      int p = thisIndex + (should_be_greater() ? -1 : 1);
+      if (p < 0) p = 0;
+      if (p >= n) p = n - 1;
+      return p;
+    }
+
+public:
+  Elem(CProxy_Main m, int s);
+  void process_num(int d);
+  void verify(int d, bool is_main);
+};
+
+Elem::Elem(CProxy_Main m, int s) : main(m), n(s)
+{
+  self = thisProxy[thisIndex];
+  num = rand();
+  thisProxy[partner(stage)].recv_num(num);
+  self.next_stage();
+}
+
+void Elem::process_num(int d)
+{
+  if ((should_be_greater() && num < d) || (!should_be_greater() && num > d))
+    num = d;
+  ++stage;
+
+  if (stage >= n) {
+    CkCallback cb(CkReductionTarget(Main, done), main);
+    contribute(0, nullptr, CkReduction::nop, cb);
+    return;
+  }
+
+  thisProxy[partner(stage)].recv_num(num);
+  self.next_stage();
+}
+
+void Elem::verify(int d, bool is_main)
+{
+  if (is_main) {
+    if (thisIndex < n - 1)
+      thisProxy[thisIndex + 1].verify(num, false);
+
+    if (thisIndex == 0) {
+      CkCallback cb(CkReductionTarget(Main, verified), main);
+      contribute(0, nullptr, CkReduction::nop, cb);
+    }
+    return;
+  }
+
+  if (d > num) {
+    ckout << "did not sort correctly at " << thisIndex
+          << " with values " << d << " " << num << endl;
+    CkExit();
+  }
+
+  CkCallback cb(CkReductionTarget(Main, verified), main);
+  contribute(0, nullptr, CkReduction::nop, cb);
+}
+
+#include "odd_even.def.h"
